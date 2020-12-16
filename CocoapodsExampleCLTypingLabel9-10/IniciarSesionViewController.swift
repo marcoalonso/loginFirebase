9967db2c3b2c987362b72bebdcb2497e7a2d2676
Marco Alonso <marcoalonsorota@hotmail.com>
2020-12-16 11:17:12 -0600

    Adding and reading data to Firestore

---
+++ CocoapodsExampleCLTypingLabel9-10/IniciarSesionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IniciarSesionViewController.swift
+//  CocoapodsExampleCLTypingLabel9-10
+//
+//  Created by marco alonso on 09/12/20.
+//
+
+import UIKit
+
+class IniciarSesionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CocoapodsExampleCLTypingLabel9-10/RegistroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegistroViewController.swift
+//  CocoapodsExampleCLTypingLabel9-10
+//
+//  Created by marco alonso on 09/12/20.
+//
+
+import UIKit
+
+class RegistroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
